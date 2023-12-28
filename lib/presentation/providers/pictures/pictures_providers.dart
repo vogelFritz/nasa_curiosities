4006af8eb333f8effa60867feb6b56ad9bc975da
fritz <franciscosantin2002@gmail.com>
2023-12-27 23:18:28 -0300

    intento de obtener apod url

--- lib/presentation/providers/pictures/pictures_providers.dart
+++ lib/presentation/providers/pictures/pictures_providers.dart
@@ -1,10 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nasa_curiosities/domain/entities/picture.dart';
+import 'package:nasa_curiosities/presentation/providers/pictures/picture_repository_provider.dart';
 
 final apodProvider = StateNotifierProvider<ApodNotifier, List<Picture>>((ref) {
-  return ApodNotifier();
+  final fetchApod = ref.watch(pictureRepositoryProvider).getApod;
+  return ApodNotifier(fetchApod: fetchApod);
 });
 
+typedef ApodCallback = Future<Picture> Function();
+
 class ApodNotifier extends StateNotifier<List<Picture>> {
-  ApodNotifier() : super([]);
+  ApodCallback fetchApod;
+  ApodNotifier({required this.fetchApod}) : super([]);
+  Future<void> getApodPicture() async {
+    final Picture apodPicture = await fetchApod();
+    state = [...state, apodPicture];
+  }
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nasa_curiosities/presentation/providers/pictures/pictures_providers.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   static const String name = 'home-screen';
   const HomeScreen({super.key});
 
   @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(apodProvider.notifier).getApodPicture();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final apod = ref.watch(apodProvider);
+    return Scaffold(
+        appBar: AppBar(title: const Text('Astronomy Picture Of The Day')),
+        body: Image.network(apod.first.url));
   }
 }
