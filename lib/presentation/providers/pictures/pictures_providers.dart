0dff5b90582acbdfba1e6a8271d4dd0faafda61e
fritz <franciscosantin2002@gmail.com>
2023-12-29 10:55:32 -0300

    mars photos provider

--- lib/presentation/providers/pictures/pictures_providers.dart
+++ lib/presentation/providers/pictures/pictures_providers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nasa_curiosities/domain/entities/mars_photo.dart';
 import 'package:nasa_curiosities/domain/entities/picture.dart';
 import 'package:nasa_curiosities/presentation/providers/pictures/picture_repository_provider.dart';
 
@@ -11,9 +12,28 @@ typedef ApodCallback = Future<Picture> Function();
 
 class ApodNotifier extends StateNotifier<List<Picture>> {
   ApodCallback fetchApod;
+
   ApodNotifier({required this.fetchApod}) : super([]);
   Future<void> getApodPicture() async {
     final Picture apodPicture = await fetchApod();
     state = [...state, apodPicture];
+  }
+}
+
+typedef MarsPhotosCallback = Future<List<MarsPhoto>> Function();
+
+final marsPhotosProvider =
+    StateNotifierProvider<MarsPhotosNotifier, List<MarsPhoto>>((ref) {
+  MarsPhotosCallback fetchMarsPhotos =
+      ref.watch(pictureRepositoryProvider).getMarsPhotos;
+  return MarsPhotosNotifier(fetchMarsPhotos: fetchMarsPhotos);
+});
+
+class MarsPhotosNotifier extends StateNotifier<List<MarsPhoto>> {
+  MarsPhotosCallback fetchMarsPhotos;
+  MarsPhotosNotifier({required this.fetchMarsPhotos}) : super([]);
+  Future<void> getMarsPhotos() async {
+    final List<MarsPhoto> marsPhotos = await fetchMarsPhotos();
+    state = [...state, ...marsPhotos];
   }
 }
